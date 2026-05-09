e05cf33655e46ae342610f9c17f859332caa8303
gsangeetha-yadav <gsangeethasgp@gmail.com>
2026-05-09 21:55:58 +0530

    Create data_type.py
    learned python basic Data Types

---
+++ data_type.py
@@ -0,0 +1,39 @@
+# program to print data type of a variable
+a=5 
+b=10.0
+c=5+6j
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+#string data type
+srt = "sangeetha"
+print(srt)
+print(type(srt))
+# list data type
+lst = ['science','maths','biology','english']
+print(lst)
+print(type(lst))
+# tuple data type
+t = (20,55,78,90,89)
+print(t)
+print(type(t))
+#set data type
+set1 = {"apple","banaba","cherry","orange"}
+print(set1)
+print(type(set1))
+# dictionary data type
+d = {"name":"sangeetha","branch":"CSE","semester":"2nd sem"}
+print(d)
+print(type(d))
+# range data type
+r = range(10)
+print(r)
+print(type(r))
+print(list(r))
+# none data type
+var = None
+print(var)
+print(type(var))
+
