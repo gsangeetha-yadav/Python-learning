d259bfaf3bbc84f1fad1e11dcee6946f4213503d
gsangeetha-yadav <gsangeethasgp@gmail.com>
2026-05-10 21:36:31 +0530

    Create operators.py
    Added program on operators in python

---
+++ operators.py
@@ -0,0 +1,39 @@
+#Arithmetic operation
+a=5
+b=8
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+#unary operator
+a=10
+print(+a)
+print(-a)
+# Relational operator
+c=8
+d=18
+print(c<d)
+print(c<=d)
+print(c>d)
+print(c!=d)
+print(c==d)
+# logical operator
+n1=10
+n2=20
+print(n1<11 and n2>18)
+print(n1>5 or n2>18)
+print(not(n1>20))
+#assignment operator
+s=20
+s+=10
+print(s)
+s-=20
+print(s)
+s*=10
+print(s)
+s/=2
+print(s)
+s%=10
+print(s)
