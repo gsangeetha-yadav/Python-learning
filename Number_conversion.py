0b3266fa5ebf0767f27e7bc1d73e8dab0260c575
gsangeetha-yadav <gsangeethasgp@gmail.com>
2026-05-17 21:23:13 +0530

    Create Number_conversion.py
    learned number system conversion in python

---
+++ Number_conversion.py
@@ -0,0 +1,9 @@
+#Number conversion in python
+num = 45
+print("Binary:",bin(45))
+print("Octal:",oct(45))
+print("hexadecimal:",hex(45))
+octal="17"
+print(int(octal,8))
+hexadecimal="1A"
+print(int(hexadecimal,16))
