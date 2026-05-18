1d3580846cda5707dfb8facb02715877c3166855
gsangeetha-yadav <gsangeethasgp@gmail.com>
2026-05-18 21:42:37 +0530

    Create Bitwise.py

---
+++ Bitwise.py
@@ -0,0 +1,22 @@
+# Basic Bitwise Operators in Python
+
+a = 10
+b = 4
+
+# AND Operator
+print("a & b =", a & b)
+
+# OR Operator
+print("a | b =", a | b)
+
+# XOR Operator
+print("a ^ b =", a ^ b)
+
+# NOT Operator
+print("~a =", ~a)
+
+# Left Shift Operator
+print("a << 1 =", a << 1)
+
+# Right Shift Operator
+print("a >> 1 =", a >> 1)
